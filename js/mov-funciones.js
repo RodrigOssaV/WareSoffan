69026ddf59738c5e4ba2d37e0e78c3086f0c6a78
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2020-07-24 15:45:26 -0400

    Up

--- js/mov-funciones.js
+++ js/mov-funciones.js
@@ -1,4 +1,9 @@
-// funciones en interacción con el usuario
+// Función que permite refrescar la página inicial
+function volverAtras(){
+    window.location = "../pages/index.html"
+}
+
+// ----- Funciones que se encargan de ingresar proveedores, conductores y productos al sistema. -----
 function clickProveedor(){
     var ventanaProveedor
     var configuracionVentanaProveedor = "menubar = yes, location = yes, resizable = yes, scrollbars = yes, status = yes"
@@ -6,33 +11,28 @@ function clickProveedor(){
     function abrirVentanaProveedor(){
         ventanaProveedor = window.open("../pages/proveedor.html", "Ingreso proveedores a WareSoffan", configuracionVentanaProveedor)
     }
-
     abrirVentanaProveedor()
 }
 
-// function clickConductor(){
-//     var ventanaConductor
-//     var configuracionVentanaConductor = "width: 1024px, height: 768px, menubar = yes, location = yes, resizable = yes, scrollbars = yes, status = yes"
+function clickConductor(){
+    var ventanaConductor
+    var configuracionVentanaConductor = "width: 1024px, height: 768px, menubar = yes, location = yes, resizable = yes, scrollbars = yes, status = yes"
 
-//     function abrirVentanaConductor() {
-//         ventanaConductor = window.open("conductor.html", "Ingreso conductores a WareSoffan", configuracionVentanaConductor)
-//       }
-//       // Llamamos a la función      
-//       abrirVentanaConductor()
-// }
-
-// function clickProducto(){
-//     var ventanaProducto
-//     var configuracionVentanaProducto = "menubar = yes, location = yes, resizable = yes, scrollbars = yes, status = yes"
-
-//     function abrirVentanaProducto(){
-//         ventanaProducto = window.open("producto.html", "", configuracionVentanaProducto)
-//     }
-
-//     abrirVentanaProducto()
-// }
-
-function volverAtras(){
-    window.location = "../pages/index.html"
+    function abrirVentanaConductor() {
+        ventanaConductor = window.open("../pages/conductor.html", "Ingreso conductores a WareSoffan", configuracionVentanaConductor)
+    }   
+      abrirVentanaConductor()
 }
-// funciones en interacción con el usuario
+
+function clickProducto(){
+    var ventanaProducto
+    var configuracionVentanaProducto = "menubar = yes, location = yes, resizable = yes, scrollbars = yes, status = yes"
+
+    function abrirVentanaProducto(){
+        ventanaProducto = window.open("../pages/producto.html", "Ingreso productos a bodega WareSoffan", configuracionVentanaProducto)
+    }
+    abrirVentanaProducto()
+}
+// ----- Funciones que se encargan de ingresar proveedores, conductores y productos al sistema. -----
+
+
