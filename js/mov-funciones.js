d71ea5c7d694bdd3212fc3e4e6d6969725fb9419
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2020-07-24 20:30:56 -0400

    Arreglo
    Por no saber usar github, quedo la escoba.

--- js/mov-funciones.js
+++ js/mov-funciones.js
@@ -1,15 +1,4 @@
-<<<<<<< Updated upstream
 // funciones en interacción con el usuario
-=======
-const { app, BrowserWindow } = require('electron')
-
-// Función que permite refrescar la página inicial
-function volverAtras(){
-    window.location = "../pages/index.html"
-}
-
-// ----- Funciones que se encargan de ingresar proveedores, conductores y productos al sistema. -----
->>>>>>> Stashed changes
 function clickProveedor(){
     var ventanaProveedor
     var configuracionVentanaProveedor = "width=480, height=700, status=1, minWidth=480, minHeight=700, maxWidth=480, maxHeight=700"
@@ -21,22 +10,16 @@ function clickProveedor(){
     abrirVentanaProveedor()
 }
 
-<<<<<<< Updated upstream
-// function clickConductor(){
-//     var ventanaConductor
-//     var configuracionVentanaConductor = "width: 1024px, height: 768px, menubar = yes, location = yes, resizable = yes, scrollbars = yes, status = yes"
-=======
 function clickConductor(){
     var ventanaConductor
     var configuracionVentanaConductor = "width=480, height=700, status=1, minWidth=480, minHeight=700, maxWidth=480, maxHeight=700"
->>>>>>> Stashed changes
 
-//     function abrirVentanaConductor() {
-//         ventanaConductor = window.open("conductor.html", "Ingreso conductores a WareSoffan", configuracionVentanaConductor)
-//       }
-//       // Llamamos a la función      
-//       abrirVentanaConductor()
-// }
+    function abrirVentanaConductor() {
+        ventanaConductor = window.open("conductor.html", "Ingreso conductores a WareSoffan", configuracionVentanaConductor)
+      }
+      // Llamamos a la función      
+      abrirVentanaConductor()
+}
 
 // function clickProducto(){
 //     var ventanaProducto
